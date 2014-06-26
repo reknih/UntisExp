c6de2948a2538e715ca0ada0b8095cb8131de4d7
Martin Haug <mhaug@live.de>
2014-06-26 20:17:28 +0200

    New Helper classes

---
+++ informant-core/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace UntisExp
+{
+	public class Helpers
+	{
+		public Helpers ()
+		{
+		}
+		public static string TruncateWithPreservation(string s, int len)
+		{
+			s = s.Replace ("\n", "");
+			string[] parts = s.Split(' ');
+			StringBuilder sb = new StringBuilder();
+
+			foreach (string part in parts)
+			{
+				if (sb.Length + part.Length > len)
+					break;
+
+				sb.Append(' ');
+				sb.Append(part);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
+
